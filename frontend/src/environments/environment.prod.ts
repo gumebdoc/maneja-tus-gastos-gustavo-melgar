614b5d0f7c3624219739abe8e744ea7f63e4eb61
gumebdoc <gmelgarb@doc.emi.edu.bo>
2026-03-18 17:30:55 -0400

    commit 2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
-  apiUrl: 'https://maneja-tus-gastos.onrender.com',
+  apiUrl: 'https://maneja-tus-gastos-api-bfl5.onrender.com',
 };
 
